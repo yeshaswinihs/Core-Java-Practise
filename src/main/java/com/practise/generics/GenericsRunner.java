6e95ec175869e08439ae8c805e12ec0809fa2e62
pradeep-kodavoor <30633187+pradeep-kodavoor@users.noreply.github.com>
2018-10-29 00:34:02 +0530

    Functional Programming practice

--- src/main/java/com/practise/generics/GenericsRunner.java
+++ src/main/java/com/practise/generics/GenericsRunner.java
@@ -1,12 +1,28 @@
 package com.practise.generics;
 
+import java.util.List;
+
 public class GenericsRunner {
 
 	public static void main(String[] args) {
+		
 		MyCustomList<String> list = new MyCustomList<>();
 		list.addElement("Element 1");
 		list.addElement("Element 2");
-
+		
+		System.out.println(list.getElement(0));
+		
+		MyCustomList<Integer> numberList = new MyCustomList<>();
+		numberList.addElement(1);
+		numberList.addElement(2);
+		
+		System.out.println(numberList.getElement(0));
+		
+		List<Integer> streamList = List.of(1,4,7,9);
+		int sum = streamList.stream().reduce( 0, (num1,num2) -> num1 + num2);
+		int sumOfOdd = streamList.stream().filter(num ->  num%2==1).reduce( 0, (num1,num2) -> num1 + num2);
+		System.out.println("Sum: "+sum+", Sum of odd:"+sumOfOdd);
+		
 	}
 
 }
--- src/main/java/com/practise/generics/MyCustomList.java
+++ src/main/java/com/practise/generics/MyCustomList.java
@@ -10,5 +10,13 @@ public class MyCustomList<T> {
 	public void addElement(T value) {
 		list.add(value);
 	}
+	
+	public void removeElement(int index) {
+		list.remove(index);
+	}
+	
+	public T getElement(int index) {
+		return list.get(index);
+	}
 
 }
