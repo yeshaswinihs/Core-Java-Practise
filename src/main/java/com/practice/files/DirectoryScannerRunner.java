a0994c62b194f8cceed20cd28e7fb33a0aef3b9f
734456 <734456@PC363420.cts.com>
2018-10-30 20:05:33 +0530

    Files practice

---
+++ src/main/java/com/practice/files/DirectoryScannerRunner.java
@@ -0,0 +1,25 @@
+package com.practice.files;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+
+public class DirectoryScannerRunner {
+
+	public static void main(String[] args) throws IOException {
+		Path currentDirectory = Paths.get(".\\src\\main\\java");
+		// Files.list(currentDirectory).forEach(System.out::println);
+
+		Predicate<? super Path> predicate = path -> String.valueOf(path).endsWith(".java");
+		// Files.walk(currentDirectory,
+		// 5).filter(predicate).forEach(System.out::println);
+
+		BiPredicate<Path, BasicFileAttributes> matcher = (path, attributes) -> String.valueOf(path).contains(".java");
+		Files.find(currentDirectory, 4, matcher).forEach(System.out::println);
+	}
+
+}
