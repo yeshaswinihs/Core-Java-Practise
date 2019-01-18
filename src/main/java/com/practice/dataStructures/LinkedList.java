ff8b4bc396d3a3d2231376590530960e017d8a9e
734456 <734456@PC363420.cts.com>
2019-01-18 18:43:01 +0530

    Linked List complete implementation

--- src/main/java/com/practice/dataStructures/LinkedList.java
+++ src/main/java/com/practice/dataStructures/LinkedList.java
@@ -18,6 +18,7 @@ public class LinkedList {
 		Node newNode = new Node(data);
 		newNode.next = null;
 
+		// Check if list is empty
 		if (list.head == null) {
 			list.head = newNode;
 		} else {
@@ -34,36 +35,63 @@ public class LinkedList {
 	void printList(LinkedList list) {
 		Node currentNode = list.head;
 		while (currentNode != null) {
-			System.out.println(currentNode.data);
+			System.out.print(currentNode.data + " ");
 			currentNode = currentNode.next;
 		}
+		System.out.println();
 
 	}
 
-	private void deleteNodeByKey(LinkedList list, int key) {
+	private void deleteNodeByValue(LinkedList list, int value) {
 
 		Node currentNode = list.head;
 		Node previousNode = null;
-		boolean keyFound = false;
-		if (currentNode.data == key) {
+		boolean valueFound = false;
+		if (currentNode.data == value) {
 			list.head = currentNode.next;
-			keyFound = true;
+			valueFound = true;
 		} else {
-			while (currentNode != null && currentNode.data != key) {
+			while (currentNode != null && currentNode.data != value) {
 				previousNode = currentNode;
 				currentNode = currentNode.next;
 			}
-			if (currentNode != null && currentNode.data == key) {
+			if (currentNode != null && currentNode.data == value) {
 				previousNode.next = currentNode.next;
-				keyFound = true;
+				valueFound = true;
 			}
 		}
 
-		if (keyFound) {
-			System.out.println("Successfully deleted key: " + key + " from the list..");
+		if (valueFound) {
+			System.out.println("Successfully deleted value: " + value + " from the list..");
 		} else {
-			System.out.println("Key not found!!");
+			System.out.println("Value not found!!");
 		}
+	}
+
+	private void deleteNodeByIndex(LinkedList list, int index) {
+
+		Node currentNode = list.head;
+		Node previousNode = null;
+		int counter = 0;
+
+		if (index == 0) {
+			list.head = currentNode.next;
+		} else {
+			while (currentNode != null && counter != index) {
+				previousNode = currentNode;
+				currentNode = currentNode.next;
+				counter++;
+			}
+
+			if (currentNode != null) {
+				previousNode.next = currentNode.next;
+				System.out.println("Element at index: " + index + " deleted succesfully..");
+			} else {
+				System.out.println("Index out of bound!!");
+			}
+
+		}
+
 	}
 
 	public static void main(String[] args) {
@@ -80,14 +108,21 @@ public class LinkedList {
 
 		list.printList(list);
 
-		list.deleteNodeByKey(list, 1);
+		list.deleteNodeByValue(list, 1);
 		System.out.println("List after deleting head");
 		list.printList(list);
-		list.deleteNodeByKey(list, 5);
-		System.out.println("List after deleting key 5");
+		list.deleteNodeByValue(list, 5);
+		System.out.println("List after deleting value 5");
 		list.printList(list);
-		System.out.println("Deleting key 5 again");
-		list.deleteNodeByKey(list, 5);
+		System.out.println("Deleting value 5 again");
+		list.deleteNodeByValue(list, 5);
+		list.printList(list);
+		System.out.println("Deleting element at index 3");
+		list.deleteNodeByIndex(list, 3);
+		list.printList(list);
+		System.out.println("Deleting element at index 5");
+		list.deleteNodeByIndex(list, 5);
+		list.printList(list);
 	}
 
 }
