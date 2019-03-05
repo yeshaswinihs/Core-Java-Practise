6b76fb2038a6fd6066ba537c87d79689a7e61320
734456 <734456@PC363420.cts.com>
2019-03-05 19:11:03 +0530

    Synchronization

--- src/main/java/com/practice/threads/SynchronizedBlockRunner.java
+++ src/main/java/com/practice/threads/SynchronizedBlockRunner.java
@@ -4,15 +4,16 @@ public class SynchronizedBlockRunner {
 
 	public static void main(String[] args) {
 
-		Display3 display = new Display3();
+		Display3 display1 = new Display3();
+		Display3 display2 = new Display3();
 
-		MyThread4 t1 = new MyThread4(display, "Rahul");
-		MyThread4 t2 = new MyThread4(display, "Rohit");
-		MyThread4 t3 = new MyThread4(display, "Dhawan");
+		MyThread4 t1 = new MyThread4(display1, "Rahul");
+		MyThread4 t2 = new MyThread4(display2, "Rohit");
+		// MyThread4 t3 = new MyThread4(display, "Dhawan");
 
 		t1.start();
 		t2.start();
-		t3.start();
+		// t3.start();
 
 	}
 
@@ -26,7 +27,10 @@ class Display3 {
 		;
 		;
 		;// Few lines of code
-		synchronized (this) {
+			// Object level synchronization
+			// synchronized (this) {
+			// Class level synchronization
+		synchronized (Display3.class) {
 			for (int i = 0; i < 10; i++) {
 				System.out.print("Good Morning: ");
 				try {
